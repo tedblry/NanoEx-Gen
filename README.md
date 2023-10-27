4913ca660f365176ac7af9b33a53b1641a255827
tedblry <104002228+tedblry@users.noreply.github.com>
2023-10-27 16:31:21 -0400

    Final version

--- README.md
+++ README.md
@@ -1,48 +1,84 @@
-# Experimental Nanopore FASTQ generator (NanoEx-Gen)
+# NanoEx-Gen: A Comprehensive Nanopore Sequencing Data Simulator for Bacterial Genome
 
-The purpose of this code is to generate a simulated FASTQ file by utilizing the read lengths extracted from an experimental FASTQ file and randomly selecting sequences from a provided reference genome. The generated reads carry equivalent lengths to the experimental reads, though originating from random positions within the reference genome.
+NanoEx-Gen is a sophisticated Python-based pipeline designed to generate simulated FASTQ files from experimental reads. The pipeline introduces errors into the simulated reads based on the error profile extracted from the alignments of the experimental reads to a reference genome. This approach ensures that the simulated reads closely mimic the error profile of experimental Oxford Nanopore Technology (ONT) sequencing data, providing a robust tool for benchmarking and evaluating bioinformatics algorithms.
+
+## Prerequisites
+
+To run NanoEx-Gen, you need to have the following dependencies installed:
+
+- Python 3
+- pysam
+
+You can install these dependencies using pip:
+
+```
+pip install pysam
+
+```
+
+## Input Preparation
+
+NanoEx-Gen requires the following inputs:
+
+1. **Experimental FASTQ File**: A FASTQ file with ONT DNA sequencing reads from an experiment.
+2. **Reference Genome**: A FASTA file of the reference genome from which you wish to simulate reads.
+3. **Aligned BAM File**: A BAM file containing the alignment of the experimental FASTQ reads against the reference genome. This file is used to derive the error profile for the simulation.
+4. **BAM Index File**: The corresponding index file (.bai) for the BAM file. This file should be located in the same directory as the BAM file.
 
 ## Usage
 
-Run the script using the following command:
+You can run NanoEx-Gen using the following command:
 
-```bash
-./nanoexgen.py path_to_infile path_to_ref_g path_to_output_simulated_fastq
+```
+./nanoexgen.py /path/to/infile /path/to/ref_g /path/to/aligned/bam /path/to/output/simulated/fastq --seed [seed_value]
+
 ```
 
-Please specify the paths to the input FASTQ file (`infile`), reference genome (`ref_g`), and the desired output location for the simulated FASTQ file (`output_simulated_fastq`).
+In this command:
 
-Example:
+- Replace `/path/to/infile` with the path to your input FASTQ file.
+- Replace `/path/to/ref_g` with the path to your reference genome.
+- Replace `/path/to/aligned/bam` with the path to your aligned BAM file.
+- Replace `/path/to/output/simulated/fastq` with the path where you want to save the simulated FASTQ file.
+- Replace `[seed_value]` with an integer to set the random seed for reproducibility.
 
-```bash
-./nanoexgen.py /path/to/experimental_data.fq /path/to/reference_genome.fasta /path/to/output/simulated_fastq.fq
+Here's an example:
+
+```
+./nanoexgen.py /path/to/experimental_data.fq /path/to/reference_genome.fasta /path/to/aligned_data.bam /path/to/output/simulated_fastq.fq --seed 12345
+
 ```
 
-To run the script, you would use a command like:
+Before running the script, make sure it has execute permissions:
 
-If you're using a UNIX-based system, execute permissions with:
-
-```bash
+```
 chmod +x nanoexgen.py
-```
-## Functions
 
-- **readfq(fp)**: Generator function to read sequences from FASTQ files.
-- **generate_simulated_fastq(infile, ref_g, output_file, seed=519)**: The reference genome is loaded into memory, and subsequently, for each read in the experimental data, a position in the reference genome is randomly chosen. From this position, a sequence of equal length is extracted. The newly generated sequence, along with the original header and quality scores, is recorded and saved in the newly created FASTQ file.
+```
+
+## Pipeline Workflow
+
+NanoEx-Gen executes the following steps:
+
+1. **Read sequences from the input FASTQ file**: For each sequence, a random start position is selected within the reference genome. If the selected start position and read length exceed the end of the reference genome, the sequence wraps around to the beginning of the genome.
+2. **Extract the error profile from the BAM file**: The error profile includes the rates of mismatches, insertions, and deletions observed in the alignments.
+3. **Introduce errors into the simulated reads**: For each base in the sequence, a random number is generated. If the random number is less than the mismatch rate, a mismatch is introduced. If it's less than the sum of the mismatch rate and the insertion rate, an insertion is introduced. If it's less than the sum of the mismatch rate, the insertion rate, and the deletion rate, a deletion is introduced.
+4. **Write the simulated reads to the output FASTQ file**: The simulated reads with introduced errors and their corresponding quality scores are written to the output FASTQ file.
 
 ## Acknowledgments
 
-Sections of this code were adopted from:
-- [readfq](https://github.com/lh3/readfq)
-- [NanoSim](https://github.com/bcgsc/NanoSim/)
-
-It's always good practice to acknowledge and attribute the original sources when utilizing or modifying code.
+This pipeline incorporates code from **[readfq](https://github.com/lh3/readfq)** and **[NanoSim](https://github.com/bcgsc/NanoSim/)**. We would like to acknowledge their contributions to the bioinformatics community.
 
 ## Citation
 
-If you use this code in your research, please cite:
+If you use NanoEx-Gen in your research, please cite:
 
 ```
 [Byeongyeon Cho], Experimental Nanopore FASTQ generator (NanoEx-Gen), GitHub repository, [2023].
 URL: [https://github.com/tedblry/NanoEx-Gen]
+
 ```
+
+## Contact
+
+For any queries or issues, please raise an issue on the GitHub repository or contact the author at [**[byeongyeon_cho@hms.harvard.edu](mailto:email_address@domain.com)**].
--- nanoexgen.py
+++ nanoexgen.py
@@ -3,19 +3,12 @@
 import argparse
 import random
 import os
+import pysam
 
 def readfq(fp):
     """
     Read sequences from FASTQ files.
     Adopted from: https://github.com/lh3/readfq
-
-    Parameters:
-    - fp: file pointer to the FASTQ file
-
-    Yields:
-    - name: sequence name/header
-    - seq: sequence
-    - qual: quality score (or None if absent)
     """
     last = None  
     while True:  
@@ -46,57 +39,109 @@ def readfq(fp):
                     yield name, seq, ''.join(seqs)
                     break
 
+def extract_error_profile(bam_file):
+    print("Extracting error profile from the BAM file...")
+    samfile = pysam.AlignmentFile(bam_file, "rb")
+    
+    total_bases = 0
+    mismatches = 0
+    insertions = 0
+    deletions = 0
+    
+    for read in samfile.fetch():
+        if hasattr(read, 'seq') and read.seq:
+            total_bases += len(read.seq)
+            for (op, length) in read.cigar:
+                if op == 1:  # insertion
+                    insertions += length
+                elif op == 2:  # deletion
+                    deletions += length
+                elif op == 4:  # soft clipping, consider these as mismatches
+                    mismatches += length
+                
+    samfile.close()
+    
+    error_profile = {
+        "mismatch_rate": mismatches / total_bases,
+        "insertion_rate": insertions / total_bases,
+        "deletion_rate": deletions / total_bases
+    }
+    
+    return error_profile
 
-def generate_simulated_fastq(infile, ref_g, output_file, seed=519):
-    """
-    Generate simulated FASTQ file using lengths of reads from an experimental FASTQ file and sequences 
-    randomly selected from a reference genome.
-    Adopted from: https://github.com/bcgsc/NanoSim/
+def introduce_errors(sequence, error_profile):
+    new_sequence = []
+    for base in sequence:
+        rand = random.random()
+        if rand < error_profile['mismatch_rate']:
+            new_sequence.append(random.choice('ACGT'.replace(base, '')))  # introduce a mismatch
+        elif rand < error_profile['mismatch_rate'] + error_profile['insertion_rate']:
+            new_sequence.append(base)
+            new_sequence.append(random.choice('ACGT'))  # introduce an insertion
+        elif rand < error_profile['mismatch_rate'] + error_profile['insertion_rate'] + error_profile['deletion_rate']:
+            continue  # introduce a deletion
+        else:
+            new_sequence.append(base)
+    return ''.join(new_sequence)
 
-    Parameters:
-    - infile: Path to the input FASTQ file
-    - ref_g: Path to the reference genome file
-    - output_file: Path to the output simulated FASTQ file
-    - seed: Random seed for reproducibility (default: 519)
-    """
+def generate_simulated_fastq(infile, ref_g, bam_file, output_file, seed=519):
+    print("Loading the reference genome...")
     with open(ref_g, 'r') as f:
         ref_sequence = ''.join([line.strip() for line in f.readlines()[1:]])
 
+    print("Setting the random seed...")
     random.seed(seed)
 
-    # Create the directory if it doesn't exist
+    print("Creating the output directory if it doesn't exist...")
     output_directory = os.path.dirname(output_file)
     if output_directory and not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    with open(output_file, 'w') as out_f, open(infile, 'r') as in_f:
-        for seq_name, seq, qual in readfq(in_f):
-            start = random.randint(0, len(ref_sequence) - len(seq))
-            new_seq = ref_sequence[start:start + len(seq)]
+    error_profile = extract_error_profile(bam_file)
 
-            # If the sequence length is 0, skip writing this entry
+    with open(output_file, 'w') as out_f, open(infile, 'r') as in_f:
+        print("Generating simulated FASTQ entries...")
+        
+        print("Introducing errors to the sequence based on the error profile...")
+        
+        for seq_name, seq, qual in readfq(in_f):
+            start = random.randint(0, len(ref_sequence))
+            
+            seq_len_over_end =  start + len(seq) - len(ref_sequence)
+            #print(f"sequence length over endpoint: {seq_len_over_end}")
+
+            if seq_len_over_end > 0:
+                new_seq = ref_sequence[start:(len(ref_sequence)+1)] + ref_sequence[0:seq_len_over_end]
+            else:
+                new_seq = ref_sequence[start:start+len(seq)]
+
             if len(new_seq) == 0:
                 continue
 
-            # Adjust the length of the quality string to match the length of the simulated sequence
+            new_seq = introduce_errors(new_seq, error_profile)
+            #print(qual)
+
             if len(new_seq) > len(qual):
-                # Extend the quality string by repeating the last character
                 qual += qual[-1] * (len(new_seq) - len(qual))
             else:
-                # Truncate the quality string
                 qual = qual[:len(new_seq)]
 
             out_f.write(f"@{seq_name}\n{new_seq}\n+\n{qual}\n")
+
+    print("Simulated FASTQ generation completed!")
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a simulated FASTQ file from experimental reads.")
     parser.add_argument("infile", help="Input FASTQ file path.")
     parser.add_argument("ref_g", help="Reference genome file path.")
+    parser.add_argument("bam_file", help="Path to the BAM file with alignments of the reads to the reference genome.")
     parser.add_argument("output_simulated_fastq", help="Output simulated FASTQ file path.")
-    parser.add_argument("--seed", type=int, default=519, help="Random seed for reproducibility (default: 519).")
+    parser.add_argument("--seed", type=int, default=519, help="Random seed for reproducibility. Default is 519.")
     args = parser.parse_args()
 
-    generate_simulated_fastq(args.infile, args.ref_g, args.output_simulated_fastq, args.seed)
+    print("Starting the generation process...")
+    generate_simulated_fastq(args.infile, args.ref_g, args.bam_file, args.output_simulated_fastq, args.seed)
+    print("Generation process completed!")
 
 if __name__ == "__main__":
     main()
